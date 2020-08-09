b197e400712fd97e43ef6cc61362c274e74b6bdb
ytx <ytxcon@foxmail.com>
2020-08-09 22:49:51 +0800

    antd pro app init

---
+++ src/pages/KnowledgeGraph/index.jsx
@@ -0,0 +1,36 @@
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import React, { useState, useEffect } from 'react';
+import { Spin,Input } from 'antd';
+import styles from './index.less';
+
+import GraphChart from './GraphChart.jsx'
+const { Search } = Input;
+
+export default () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+  return (
+    <PageHeaderWrapper className={styles.main}>
+      <div
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        <Search
+            placeholder="input search text"
+            enterButton="Search"
+            size="large"
+            onSearch={value => console.log(value)}
+          />
+        <GraphChart></GraphChart>
+        <Spin spinning={loading} size="large" >
+          
+        </Spin>
+      </div>
+    </PageHeaderWrapper>
+  );
+};
