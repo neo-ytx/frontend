c218489e591ef5be9cfe9d659f5e40abbdea6c9e
ytx <ytxcon@foxmail.com>
2020-08-17 23:45:11 +0800

    upload file

--- src/pages/ListTableList/index.jsx
+++ src/pages/ListTableList/index.jsx
@@ -74,18 +74,19 @@ const handleRemove = async selectedRows => {
 
 const TableList = () => {
   const uploadProps = {
+    // withCredentials: true,
     name: 'file',
     multiple: true,
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    action: '/api/rule',
     onChange(info) {
       const { status } = info.file;
       if (status !== 'uploading') {
         console.log(info.file, info.fileList);
       }
       if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`);
+        message.success(`${info.file.name} 文件上传成功。`);
       } else if (status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
+        message.error(`${info.file.name} 文件上传失败。`);
       }
     },
   };
